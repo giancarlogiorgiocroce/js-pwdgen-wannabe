48bd55f98c2f4507011fcae50bebb50cd7f56f6a
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-05 15:01:23 +0200

    Fine esercizio

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ Infine scrivi sulla pagina nomecognomecolorepreferito22
 */
 
 console.log('hello world');
+
+const firstName = prompt("Digita il tuo nome");
+const lastName = prompt("Digita il tuo cognome");
+const favoriteColor = prompt("Digita il tuo colore preferito");
+
+const newPassword = firstName + lastName + favoriteColor + '22';
+
+document.getElementById('new-password').innerHTML += newPassword;
